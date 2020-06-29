77783b66b14030e0b6d2f4ea0ffd227ecd21d07b
Leo <leo.guo@aisushi.com.cn>
2020-06-29 10:15:22 +0800

    初始化实例代码

---
+++ src/NC.ProxyService/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NC.ProxyService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ src/NC.UpgradeService/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NC.UpgradeService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
